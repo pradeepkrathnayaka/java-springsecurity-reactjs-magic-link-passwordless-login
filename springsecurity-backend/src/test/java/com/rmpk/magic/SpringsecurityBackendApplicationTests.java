f733ced9d807406f92c62b9035df8631934c3669
Pradeep Rathnayaka <pradeepkrathnayaka@gmail.com>
2026-04-09 12:53:34 +0800

    include project setup

---
+++ springsecurity-backend/src/test/java/com/rmpk/magic/SpringsecurityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmpk.magic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
